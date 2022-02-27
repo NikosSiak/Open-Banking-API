b87c959df41f36793040ff28643e46c89963a72b
NickSiak <nikos.siakas@gmail.com>
2022-02-27 19:49:29 +0200

    Add CORS global middleware

--- lib/request_handler.go
+++ lib/request_handler.go
@@ -10,5 +10,20 @@ type RequestHandler struct {
 
 func NewRequestHandler() RequestHandler {
   engine := gin.New()
+  engine.Use(CORSMiddleware)
   return RequestHandler{Gin: engine}
+}
+
+func CORSMiddleware(c *gin.Context) {
+  c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+  c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+  c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+  c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+  if c.Request.Method == "OPTIONS" {
+      c.AbortWithStatus(204)
+      return
+  }
+
+  c.Next()
 }
