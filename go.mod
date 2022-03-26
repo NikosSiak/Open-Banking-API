f655105f7f9037049b18ef3e722faa5c8981e440
NikosSiak <nikos.siakas@gmail.com>
2022-03-26 21:49:00 +0200

    Add tests for user model

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.3.0
 	github.com/google/uuid v1.3.0
 	github.com/spf13/viper v1.8.0
+	github.com/stretchr/testify v1.7.1
 	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
 	github.com/swaggo/gin-swagger v1.4.1
 	github.com/swaggo/swag v1.8.0
@@ -22,6 +23,7 @@ require (
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -49,6 +51,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -71,4 +74,5 @@ require (
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ models/user_test.go
@@ -0,0 +1,106 @@
+package models_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/NikosSiak/Open-Banking-API/models"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func TestHashPassword(t *testing.T) {
+	password := "test password"
+	user := models.User{Password: password}
+
+	err := user.HashPassword()
+	assert.Nil(t, err)
+
+	assert.NotEqual(t, password, user.Password, "user password did not change")
+}
+
+func TestCheckPasswordHash(t *testing.T) {
+	password := "test password"
+	user := models.User{Password: password}
+	user.HashPassword()
+
+	assert.True(t, user.CheckPasswordHash(password), "hashed password did not match with the original password")
+}
+
+type GetBSONSuite struct {
+	suite.Suite
+	user          models.User
+	userCreatedAt primitive.DateTime
+	userUpdatedAt primitive.DateTime
+}
+
+func (suite *GetBSONSuite) SetupTest() {
+	user := models.User{}
+
+	suite.user = user
+	suite.userCreatedAt = user.CreatedAt
+	suite.userUpdatedAt = user.UpdatedAt
+}
+
+func (suite *GetBSONSuite) TestCreatedAtGetsSet() {
+	userBson := suite.user.GetBSON()
+
+	suite.NotEqual(suite.userCreatedAt, userBson.(*models.User).CreatedAt, "user created at time did not change")
+}
+
+func (suite *GetBSONSuite) TestCreatedAtDoesNotChange() {
+	createdTime := primitive.NewDateTimeFromTime(time.Now().UTC())
+	suite.user.CreatedAt = createdTime
+	suite.userCreatedAt = createdTime
+
+	userBson := suite.user.GetBSON()
+
+	suite.Equal(suite.userCreatedAt, userBson.(*models.User).CreatedAt, "user created at time did change while it was already set")
+}
+
+func (suite *GetBSONSuite) TestUpdatedAtChanges() {
+	userBson := suite.user.GetBSON()
+
+	suite.NotEqual(suite.userUpdatedAt, userBson.(*models.User).UpdatedAt, "user updated at did not change")
+}
+
+func TestGetBSONSuite(t *testing.T) {
+	suite.Run(t, new(GetBSONSuite))
+}
+
+type AddAccountSuite struct {
+	suite.Suite
+	user        models.User
+	accountName string
+	account     models.Account
+}
+
+func (suite *AddAccountSuite) SetupTest() {
+	user := models.User{}
+
+	suite.user = user
+	suite.accountName = "testAccount"
+	suite.account = models.Account{AccessToken: "access_token", RefreshToken: "refresh_token"}
+}
+
+func (suite *AddAccountSuite) TestAddNewAccount() {
+	suite.user.AddAccount(suite.accountName, &suite.account)
+	suite.Equal(&suite.account, suite.user.Accounts[suite.accountName], "account was not added")
+}
+
+func (suite *AddAccountSuite) TestAddMoreAccounts() {
+	suite.user.Accounts = map[string]*models.Account{suite.accountName: &suite.account}
+
+	secondAccountName := "testAccount2"
+	secondAccount := models.Account{}
+
+	suite.user.AddAccount(secondAccountName, &secondAccount)
+
+	suite.NotNil(suite.user.Accounts[suite.accountName], "previous account was overwritten")
+	suite.Equal(&secondAccount, suite.user.Accounts[secondAccountName], "second account was not added")
+}
+
+func TestAddAccountSuite(t *testing.T) {
+	suite.Run(t, new(AddAccountSuite))
+}
